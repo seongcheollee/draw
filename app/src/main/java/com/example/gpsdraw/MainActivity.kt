c088406dd714cb161fdbdd537269476b9598ca18
seongcheollee <jhk6520736@gmail.com>
2023-05-06 15:13:41 +0900

    viewPager2 프래그먼트 이동 간단구현

--- app/src/main/java/com/example/gpsdraw/MainActivity.kt
+++ app/src/main/java/com/example/gpsdraw/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.gpsdraw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentLsit = listOf(menuFragment1(),menuFragment2())
+        val viewPager_menu : ViewPager2 = findViewById(R.id.viewPager_menu)
+        val adapter = ViewPagerAdapter(this) // 어댑터 생성
+        adapter.fragments = fragmentLsit
+        viewPager_menu.adapter = adapter
     }
 }
