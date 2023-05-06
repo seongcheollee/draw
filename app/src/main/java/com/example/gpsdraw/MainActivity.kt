3abb78e8b933f298a3c80a8710c7a3a3c2cb2447
seongcheollee <jhk6520736@gmail.com>
2023-05-06 19:44:40 +0900

    하단 네비게이션바 추가

--- app/src/main/java/com/example/gpsdraw/MainActivity.kt
+++ app/src/main/java/com/example/gpsdraw/MainActivity.kt
@@ -2,18 +2,48 @@ package com.example.gpsdraw
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentActivity
+import android.util.Log
+import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
+import com.example.gpsdraw.adapter.ViewPagerAdapter
+import com.example.gpsdraw.fragment.GpsFragment
+import com.example.gpsdraw.fragment.MainFragment
+import com.example.gpsdraw.fragment.MyInfoFragment
+import com.example.gpsdraw.fragment.SearchFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragmentLsit = listOf(menuFragment1(),menuFragment2())
-        val viewPager_menu : ViewPager2 = findViewById(R.id.viewPager_menu)
-        val adapter = ViewPagerAdapter(this) // 어댑터 생성
-        adapter.fragments = fragmentLsit
-        viewPager_menu.adapter = adapter
+        val homeFragment = MainFragment()
+        val gpsFragment = GpsFragment()
+        val mypageFragment = MyInfoFragment()
+        val searchFragment = SearchFragment()
+
+        // 네비게이션 바 설정
+        var bnv = findViewById<BottomNavigationView>(R.id.bottomNavi)
+        bnv.setOnItemSelectedListener { MenuItem ->
+            when (MenuItem.itemId) {
+                R.id.homeFragment -> replaceFragment(homeFragment)
+                R.id.gpsFragment -> replaceFragment(gpsFragment)
+                R.id.myPageFragment -> replaceFragment(mypageFragment)
+                R.id.searchFragment -> replaceFragment(searchFragment)
+
+            }
+            true
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        Log.d("MainActivity", "${fragment}")
+        supportFragmentManager.beginTransaction()
+            .apply {
+                replace(R.id.fragmentContainerView, fragment)
+                commit()
+            }
     }
 }
--- app/src/main/java/com/example/gpsdraw/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/gpsdraw/adapter/ViewPagerAdapter.kt
@@ -1,4 +1,4 @@
-package com.example.gpsdraw
+package com.example.gpsdraw.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
