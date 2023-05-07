7bcdff281a799103434982483f0fb6e9c1168393
seongcheollee <jhk6520736@gmail.com>
2023-05-07 20:07:24 +0900

    naver map api 적용

--- app/src/main/java/com/example/gpsdraw/fragment/GpsFragment.kt
+++ app/src/main/java/com/example/gpsdraw/fragment/GpsFragment.kt
@@ -1,22 +1,45 @@
 package com.example.gpsdraw.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.gpsdraw.R
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.util.FusedLocationSource
 
-class GpsFragment : Fragment() {
 
+class GpsFragment : Fragment() , OnMapReadyCallback {
+
+    private lateinit var mapView: MapView
+    private val LOCATION_PERMISSTION_REQUEST_CODE: Int = 1000
+    private lateinit var locationSource: FusedLocationSource // 위치를 반환하는 구현체
+    private lateinit var naverMap: NaverMap
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gps, container, false)
+
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSTION_REQUEST_CODE)
+
+        val rootView: View = inflater.inflate(R.layout.fragment_gps, container, false)
+        return rootView
     }
+
+
+
+    override fun onMapReady(gMap: GoogleMap) {
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+
+    }
+
 
 
 }
