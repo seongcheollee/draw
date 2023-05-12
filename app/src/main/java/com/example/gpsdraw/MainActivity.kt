81d166fb858a61786aeed1f69dd37ba328bc510c
seongcheollee <jhk6520736@gmail.com>
2023-05-12 14:21:01 +0900

    naverMap 적용, 현재 위치 불러오기, 마커 구현

--- app/src/main/java/com/example/gpsdraw/MainActivity.kt
+++ app/src/main/java/com/example/gpsdraw/MainActivity.kt
@@ -1,40 +1,109 @@
 package com.example.gpsdraw
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.annotation.UiThread
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import androidx.viewpager2.widget.ViewPager2
-import com.example.gpsdraw.adapter.ViewPagerAdapter
-import com.example.gpsdraw.fragment.GpsFragment
 import com.example.gpsdraw.fragment.MainFragment
 import com.example.gpsdraw.fragment.MyInfoFragment
 import com.example.gpsdraw.fragment.SearchFragment
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.util.FusedLocationSource
+import com.naver.maps.map.util.MarkerIcons
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var naverMap: NaverMap
+    private lateinit var mapView: MapView
+    private var LOCATION_PERMISSION = 1004
+    private val marker = Marker()
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val PERMISSION = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION ,android.Manifest.permission.ACCESS_COARSE_LOCATION )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
+        mapView = findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationSource = FusedLocationSource(this@MainActivity , LOCATION_PERMISSION)
+        val fm = supportFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.mapView) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.mapView, it).commit()
+            }
+        mapFragment.getMapAsync(this)
+
+
         val homeFragment = MainFragment()
-        val gpsFragment = GpsFragment()
         val mypageFragment = MyInfoFragment()
         val searchFragment = SearchFragment()
 
         // 네비게이션 바 설정
         var bnv = findViewById<BottomNavigationView>(R.id.bottomNavi)
+
+
         bnv.setOnItemSelectedListener { MenuItem ->
             when (MenuItem.itemId) {
-                R.id.homeFragment -> replaceFragment(homeFragment)
-                R.id.gpsFragment -> replaceFragment(gpsFragment)
-                R.id.myPageFragment -> replaceFragment(mypageFragment)
                 R.id.searchFragment -> replaceFragment(searchFragment)
+                R.id.homeFragment -> replaceFragment(homeFragment)
+                R.id.myPageFragment -> replaceFragment(mypageFragment)
+
 
             }
             true
         }
 
+
+
+    }
+
+    @UiThread
+    //NaverMap 객체는 오직 콜백 메서드를 이용해서 얻어올 수 있습니다.
+    override fun onMapReady(map: NaverMap) {
+        naverMap = map
+        val cameraPosition = CameraPosition(
+            LatLng(37.5666102, 126.9783881), // 대상 지점
+            16.0, // 줌 레벨
+            0.0, // 기울임 각도
+            180.0 // 베어링 각도
+        )
+        naverMap.cameraPosition = cameraPosition
+
+        naverMap.addOnCameraChangeListener { reason, animated ->
+            // 마커 포지션
+            marker.position =
+                LatLng(naverMap.cameraPosition.target.latitude, naverMap.cameraPosition.target.longitude) }
+
+        naverMap.addOnCameraIdleListener {
+            // 현재 보이는 네이버맵의 정중앙 가운데로 마커
+            marker.map = naverMap
+            marker.width = 50
+            marker.height = 80
+            marker.icon = MarkerIcons.BLACK
+            marker.iconTintColor = Color.BLUE
+
+        }
+        naverMap.locationSource = locationSource
+        ActivityCompat.requestPermissions(this, PERMISSION, LOCATION_PERMISSION)
+
+        val uiSettings = naverMap.uiSettings
+        uiSettings.isLocationButtonEnabled = true
+        uiSettings.isCompassEnabled = false
+        uiSettings.isZoomControlEnabled = true
 
     }
 
@@ -45,5 +114,62 @@ class MainActivity : AppCompatActivity() {
                 replace(R.id.fragmentContainerView, fragment)
                 commit()
             }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when {
+            requestCode != LOCATION_PERMISSION -> {
+                return
+            }
+            else -> {
+                when {
+                    locationSource.onRequestPermissionsResult(requestCode,permissions,grantResults) -> {
+                        if (!locationSource.isActivated){
+                            naverMap.locationTrackingMode = LocationTrackingMode.None
+                        }else{
+                            naverMap.locationTrackingMode = LocationTrackingMode.Follow
+                        }
+                    }
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 }
