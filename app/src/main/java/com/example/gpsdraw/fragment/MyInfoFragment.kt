d9b88b16250f534ba390abf989d315dfae4eace5
seongcheollee <jhk6520736@gmail.com>
2023-05-15 18:22:55 +0900

    페이지 수정

--- app/src/main/java/com/example/gpsdraw/fragment/MyInfoFragment.kt
+++ app/src/main/java/com/example/gpsdraw/fragment/MyInfoFragment.kt
@@ -5,16 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.gpsdraw.R
+import com.example.gpsdraw.adapter.GridAdapter
+import com.example.gpsdraw.adapter.UserPostAdapter
+import com.example.gpsdraw.data.RoadImages
+import com.example.gpsdraw.data.UserPostData
 
 class MyInfoFragment : Fragment() {
+
+    private var gridLayoutManager: RecyclerView.LayoutManager? = null
+    private var recyclerAdapter: RecyclerView.Adapter<GridAdapter.ViewHolder>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_info, container, false)
+
+        var imglist = arrayListOf<RoadImages>(
+            RoadImages(""),
+            RoadImages(""),
+            RoadImages(""),
+            RoadImages(""),
+            RoadImages(""),
+            RoadImages(""),
+            RoadImages(""),
+            RoadImages(""),
+            RoadImages(""),
+            RoadImages(""),
+            )
+
+
+        val view = inflater!!.inflate(R.layout.fragment_my_info, container, false)
+        val recyclerView: RecyclerView = view.findViewById(R.id.myGridView)
+
+        recyclerAdapter = GridAdapter(imglist)
+        gridLayoutManager =  GridLayoutManager(activity, 3)
+
+        recyclerView.layoutManager = gridLayoutManager
+        recyclerView.adapter = recyclerAdapter
+        return view
     }
 
 }
