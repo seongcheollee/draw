c70697770caadf4db2ed577537ebd2c5d6e2b142
seongcheollee <jhk6520736@gmail.com>
2023-08-15 15:01:08 +0900

    뷰페이저 적용된 리사이클러뷰 적용

--- app/src/main/java/com/example/gpsdraw/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/gpsdraw/adapter/ViewPagerAdapter.kt
@@ -1,18 +1,31 @@
 package com.example.gpsdraw.adapter
 
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class ViewPagerAdapter(frag : FragmentActivity) : FragmentStateAdapter(frag){
-    var fragments = listOf<Fragment>()
-    override fun getItemCount(): Int {
-        return fragments.size
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gpsdraw.databinding.ItemViewBinding
+
+class ViewPagerAdapter : RecyclerView.Adapter<ViewPagerAdapter.ViewHolder>() {
+
+    lateinit var items: ArrayList<String>
+
+    fun build(i: ArrayList<String>): ViewPagerAdapter {
+        items = i
+        return this
     }
 
-    override fun createFragment(position: Int): Fragment {
-       return fragments.get(position)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewPagerAdapter.ViewHolder =
+        ViewHolder(ItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewPagerAdapter.ViewHolder, position: Int) {
+        holder.bind(items[position])
     }
-
-
+    class ViewHolder(val binding: ItemViewBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: String) {
+            binding.tvColor.text = item
+        }
+    }
 }
--- app/src/main/java/com/example/gpsdraw/fragment/MainFragment.kt
+++ app/src/main/java/com/example/gpsdraw/fragment/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.example.gpsdraw.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,35 +11,45 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.gpsdraw.R
 import com.example.gpsdraw.adapter.UserPostAdapter
 import com.example.gpsdraw.data.UserPostData
+import com.example.gpsdraw.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
 
-    private var linearLayoutManager: RecyclerView.LayoutManager? = null
-    private var recyclerAdapter: RecyclerView.Adapter<UserPostAdapter.ViewHolder>? = null
-
+    private var _binding: FragmentMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        val view = binding.root
 
+        var nList = arrayListOf("1", "2", "3", "4")
 
         var postList = arrayListOf<UserPostData>(
-            UserPostData("nickname1","Hello, world!")
-            ,UserPostData("nickname2", "Hello, world!"),
-            UserPostData("nickname3", "Hello, world!"),
-            UserPostData("nickname4", "Hello, world!"))
+            UserPostData("잔선철","맥주 ㄱ?!", nList),
+            UserPostData("잔든스유니","아~..!",nList),
+            UserPostData("잔자라잔수현","뭐",nList),
+            UserPostData("멍멍이","Hello, world!",nList),
+            )
 
+        _binding!!.userPostView.apply {
+            adapter = UserPostAdapter().build(postList)
+            layoutManager =
+                LinearLayoutManager(activity, RecyclerView.HORIZONTAL, false)
 
-        val view = inflater!!.inflate(R.layout.fragment_main, container, false)
-        val recyclerView: RecyclerView = view.findViewById(R.id.userPostView)
+        }
 
-        recyclerAdapter = UserPostAdapter(postList)
-        linearLayoutManager =  LinearLayoutManager(activity)
+        // NonSwipeableViewPager로 대체
+        binding.userPostView.adapter = UserPostAdapter().build(postList)
+        binding.userPostView.layoutManager =
+            LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
 
-        recyclerView.layoutManager = linearLayoutManager
-        recyclerView.adapter = recyclerAdapter
 
         return view
     }
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
